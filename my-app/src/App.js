1c5c8e11559fbd0734f9852f6b00ad73b2af86e1
nehmea <79180098+nehmea@users.noreply.github.com>
2022-11-24 12:28:49 -0500

    pasted our project in my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomePage from "./containers/HomePage";
+import DashboardPage from "./containers/DashboardPage";
+import LoginPage from "./containers/LoginPage";
+import RegisterPage from "./containers/RegisterPage";
+import JobPosting from "./pages/JobPosting";
+import MyCVPage from "./containers/MyCVPage";
+
+import { store } from "./store";
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/jobpostings" element={<JobPosting />} />
+        <Route path="/cv" element={<MyCVPage />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
